3dcd6c0bcf79e171942279fcdce172c4bbfa4a03
Brigita <97016501+brimac2@users.noreply.github.com>
2022-03-02 21:43:45 +0200

    Update Basics.java

--- src/lt/academy/java/basics/Basics.java
+++ src/lt/academy/java/basics/Basics.java
@@ -2,7 +2,17 @@
 package lt.academy.java.basics;
 
 public class Basics {
-
+//Kaip islygiuoti???????
     public static void main(String[] args) {
+        int number = 5;
+        String levels = String.valueOf('#');
+        String trunk = "#";
+
+        for(int i = 0; i < number; i++){
+            System.out.println(levels);
+            levels += trunk;
+        }
+
+        System.out.println(trunk);
     }
 }
