18dbd6409d3ab016a53a196641d7220485a1971d
Brigita <97016501+brimac2@users.noreply.github.com>
2022-03-02 20:23:19 +0200

    Initial commit

---
+++ src/lt/academy/java/basics/Basics.java
@@ -0,0 +1,14 @@
+//Task 1
+package lt.academy.java.basics;
+
+public class Basics {
+    public static void main(String[] args) {
+        String name = "Karolis";
+        String surname = "Grigonis";
+        String age = "33";
+        System.out.println("Hello I am "+ name+" " + surname +"." + " " + "I am attending IT Academy 2020!");
+    }
+
+
+
+}
