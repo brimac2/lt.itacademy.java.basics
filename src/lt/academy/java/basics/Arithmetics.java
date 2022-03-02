156493bbb85911e1a1a15583d7350cc8660a6e87
Brigita <97016501+brimac2@users.noreply.github.com>
2022-03-02 22:04:05 +0200

    Create Arithmetics.java

---
+++ src/lt/academy/java/basics/Arithmetics.java
@@ -0,0 +1,10 @@
+package lt.academy.java.basics;
+
+public class Arithmetics {
+    public static void main(String[] args) {
+        int side = 5;
+        System.out.println("Cube’s volume: " + Math.pow(side, 3) + "cm3");
+        System.out.println("Cube’s perimeter: " + (side*3*4) + "cm");
+
+    }
+}
