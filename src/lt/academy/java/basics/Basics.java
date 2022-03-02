b62f77a8e6dd925c0ae5fa365c7284b8d7cda7d4
Brigita <97016501+brimac2@users.noreply.github.com>
2022-03-02 21:36:51 +0200

    Update Basics.java

--- src/lt/academy/java/basics/Basics.java
+++ src/lt/academy/java/basics/Basics.java
@@ -2,19 +2,7 @@
 package lt.academy.java.basics;
 
 public class Basics {
-//    ar gera praktika cia privite string daryti ar reikejo palikt po main?
-    private static String hello = "H3110";
-    private static String world = "w0r1d";
-    private static float two = 2.0F;
-    private static boolean variableTrue = true;
-    private static String space = " ";
 
     public static void main(String[] args) {
-//        String name = "Karolis";
-//        String surname = "Grigonis";
-//        String age = "33";
-//        System.out.println("Hello I am "+ name+" " + surname +"." + " " + "I am attending IT Academy 2020!");
-
-        System.out.println(hello+space+world+space+two+space+variableTrue);
     }
 }
