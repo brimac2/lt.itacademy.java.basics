73377b93b8176949a1a7cf228bb52d0c0fd1ad1b
Brigita <97016501+brimac2@users.noreply.github.com>
2022-03-03 10:42:24 +0200

    Update Loops.java

--- src/lt/academy/java/basics/Loops.java
+++ src/lt/academy/java/basics/Loops.java
@@ -2,20 +2,32 @@ package lt.academy.java.basics;
 
 public class Loops {
     public static void main(String[] args) {
-        int daysPerYear = 365;
-        int hoursPerOneDay = 24;
-        int minutesPerOneDay = 60*hoursPerOneDay;
+//        int daysPerYear = 365;
+//        int hoursPerOneDay = 24;
+//        int minutesPerOneDay = 60*hoursPerOneDay;
+//
+//        int minutesPerYear = 0;
+//        int hoursPerYear = 0;
+//
+//        int i = 0;
+//        while (i<daysPerYear) {
+//            minutesPerYear += minutesPerOneDay;
+//            hoursPerYear += hoursPerOneDay;
+//            i++;
+//        }
+//
+//        System.out.println("There are " + hoursPerYear + " hours or " + minutesPerYear + " minutes in one year.");
 
-        int minutesPerYear = 0;
-        int hoursPerYear = 0;
+        int factorialNumber = 5;
+        int factorialSum = factorialNumber;
+        int cycleCount = factorialNumber - 1;
+        int baseFactorial = factorialNumber;
 
-        int i = 0;
-        while (i<daysPerYear) {
-            minutesPerYear += minutesPerOneDay;
-            hoursPerYear += hoursPerOneDay;
-            i++;
+        for (int i = 0; i < cycleCount; i++){
+                factorialSum = factorialSum * --factorialNumber;
         }
-        System.out.println("There are " + hoursPerYear + " hours or " + minutesPerYear + " minutes in one year.");
-
+        System.out.println("Factorial of number " + baseFactorial  + " is " + factorialSum);
     }
 }
+
+
